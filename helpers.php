cfb2df42f847931ac66d64524d71820806b9d656
Jean-Simon de Witt <jeansimon.dewitt@gmail.com>
2024-10-07 17:21:52 +0200

    UI split into partials

--- helpers.php
+++ helpers.php
@@ -8,3 +8,34 @@
     function basePath($path = '') {
         return __DIR__ . '/' . $path;
     }
+
+    /**
+     * Load a view
+     * @param string $name
+     * @return void
+     */
+    function loadView($name) {
+       $viewPath = basePath("views/{$name}.php");
+
+       if(file_exists($viewPath)) {
+        require $viewPath;
+       }
+       else {
+        echo "View '{$name}' not found!";
+       }
+    }
+
+    /**
+     * @param string name
+     * @return void
+     */
+    function loadPartial($name) {
+        $partialPath = basePath("views/partials/{$name}.php");
+
+        if(file_exists($partialPath)) {
+            require $partialPath;
+        }
+        else {
+            echo "Partial '{$name}' not found!";
+        }
+    }
